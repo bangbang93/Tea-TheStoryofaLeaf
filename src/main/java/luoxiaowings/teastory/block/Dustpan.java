3112212f5e57220d22ef281c8ac95305f8779abd
LuoXiao-Wings <644719932@qq.com>
2016-03-19 15:42:51 +0800

    合并TeaLeaf和WetTea的部分代码至Dustpan

--- src/main/java/luoxiaowings/teastory/block/Dustpan.java
+++ src/main/java/luoxiaowings/teastory/block/Dustpan.java
@@ -9,9 +9,11 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 
@@ -35,5 +37,32 @@ public class Dustpan extends Block
 	public boolean isFullCube()
     {
         return false;
+    }
+	
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        if (worldIn.isRemote)
+        {
+            return true;
+        }
+        else
+        {
+        	if (playerIn.getHeldItem().getItem() == ItemLoader.tea_leaf && playerIn.getHeldItem().stackSize >=8)
+            	{
+        	    	worldIn.setBlockState(pos, BlockLoader.dustpan_full.getDefaultState());
+        	    	playerIn.getHeldItem().stackSize = playerIn.getHeldItem().stackSize - 8;
+                    return true;
+            	}
+        	else 
+        	{
+        		if (playerIn.getHeldItem().getItem() == ItemLoader.wet_tea && playerIn.getHeldItem().stackSize >=8)
+            	{
+        	    	worldIn.setBlockState(pos, BlockLoader.dustpan_wet.getDefaultState());
+        	    	playerIn.getHeldItem().stackSize = playerIn.getHeldItem().stackSize - 8;
+                    return true;
+            	}
+        		else return false;
+        	}
+        }
     }
 }
